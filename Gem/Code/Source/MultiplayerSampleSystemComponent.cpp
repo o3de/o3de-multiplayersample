a9c9a11fc4e638727d425d170689223a270f24d0
puvvadar <puvvadar@amazon.com>
2021-05-07 19:15:39 -0700

    Setup repository content

---
+++ Gem/Code/Source/MultiplayerSampleSystemComponent.cpp
@@ -0,0 +1,125 @@
+/*
+ * All or portions of this file Copyright (c) Amazon.com, Inc. or its affiliates or
+ * its licensors.
+ *
+ * For complete copyright and license terms please see the LICENSE at the root of this
+ * distribution (the "License"). All use of this software is governed by the License,
+ * or, if provided, by the license below or the license accompanying this file. Do not
+ * remove or modify any license notices. This file is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *
+ */
+
+#include <AzCore/Serialization/SerializeContext.h>
+#include <AzCore/Serialization/EditContext.h>
+#include <AzCore/Serialization/EditContextConstants.inl>
+
+#include <AzNetworking/Framework/INetworking.h>
+
+#include "MultiplayerSampleSystemComponent.h"
+
+
+namespace MultiplayerSample
+{
+    using namespace AzNetworking;
+
+    static const AZStd::string_view s_networkInterfaceName("MultiplayerSampleInterface");
+
+    void MultiplayerSampleSystemComponent::Reflect(AZ::ReflectContext* context)
+    {
+        if (AZ::SerializeContext* serialize = azrtti_cast<AZ::SerializeContext*>(context))
+        {
+            serialize->Class<MultiplayerSampleSystemComponent, AZ::Component>()
+                ->Version(0)
+                ;
+
+            if (AZ::EditContext* ec = serialize->GetEditContext())
+            {
+                ec->Class<MultiplayerSampleSystemComponent>("MultiplayerSample", "[Description of functionality provided by this System Component]")
+                    ->ClassElement(AZ::Edit::ClassElements::EditorData, "")
+                        ->Attribute(AZ::Edit::Attributes::AppearsInAddComponentMenu, AZ_CRC("System"))
+                        ->Attribute(AZ::Edit::Attributes::AutoExpand, true)
+                    ;
+            }
+        }
+    }
+
+    void MultiplayerSampleSystemComponent::GetProvidedServices(AZ::ComponentDescriptor::DependencyArrayType& provided)
+    {
+        provided.push_back(AZ_CRC_CE("MultiplayerSampleService"));
+    }
+
+    void MultiplayerSampleSystemComponent::GetIncompatibleServices(AZ::ComponentDescriptor::DependencyArrayType& incompatible)
+    {
+        incompatible.push_back(AZ_CRC_CE("MultiplayerSampleService"));
+    }
+
+    void MultiplayerSampleSystemComponent::GetRequiredServices(AZ::ComponentDescriptor::DependencyArrayType& required)
+    {
+        required.push_back(AZ_CRC_CE("NetworkingService"));
+        required.push_back(AZ_CRC_CE("MultiplayerService"));
+    }
+
+    void MultiplayerSampleSystemComponent::GetDependentServices(AZ::ComponentDescriptor::DependencyArrayType& dependent)
+    {
+        AZ_UNUSED(dependent);
+    }
+
+    void MultiplayerSampleSystemComponent::Init()
+    {
+        ;
+    }
+
+    void MultiplayerSampleSystemComponent::Activate()
+    {
+        AZ::TickBus::Handler::BusConnect();
+    }
+
+    void MultiplayerSampleSystemComponent::Deactivate()
+    {
+        AZ::TickBus::Handler::BusDisconnect();
+    }
+
+    void MultiplayerSampleSystemComponent::OnTick([[maybe_unused]] float deltaTime, [[maybe_unused]] AZ::ScriptTimePoint time)
+    {
+        ;
+    }
+
+    int MultiplayerSampleSystemComponent::GetTickOrder()
+    {
+        // Tick immediately after the multiplayer system component
+        return AZ::TICK_PLACEMENT + 2;
+    }
+
+    bool MultiplayerSampleSystemComponent::HandleRequest([[maybe_unused]] IConnection* connection,
+        [[maybe_unused]] const IPacketHeader& packetHeader, [[maybe_unused]] const MultiplayerSamplePackets::Sample& packet)
+    {
+        return true;
+    }
+
+    ConnectResult MultiplayerSampleSystemComponent::ValidateConnect([[maybe_unused]] const IpAddress& remoteAddress,
+        [[maybe_unused]] const IPacketHeader& packetHeader, [[maybe_unused]] ISerializer& serializer)
+    {
+        return ConnectResult::Accepted;
+    }
+
+    void MultiplayerSampleSystemComponent::OnConnect([[maybe_unused]] IConnection* connection)
+    {
+        ;
+    }
+
+    bool MultiplayerSampleSystemComponent::OnPacketReceived([[maybe_unused]] IConnection* connection, [[maybe_unused]] const IPacketHeader& packetHeader, [[maybe_unused]] ISerializer& serializer)
+    {
+        return true;
+    }
+
+    void MultiplayerSampleSystemComponent::OnPacketLost([[maybe_unused]] IConnection* connection, [[maybe_unused]] PacketId packetId)
+    {
+        ;
+    }
+
+    void MultiplayerSampleSystemComponent::OnDisconnect(IConnection* connection, [[maybe_unused]] DisconnectReason reason, [[maybe_unused]] AzNetworking::TerminationEndpoint endpoint)
+    {
+        AZLOG_INFO("Disconnected from remote address: %s", connection->GetRemoteAddress().GetString().c_str());
+    }
+}
---
+++ Gem/Code/tool_dependencies.cmake
@@ -0,0 +1,65 @@
+#
+# All or portions of this file Copyright (c) Amazon.com, Inc. or its affiliates or
+# its licensors.
+#
+# For complete copyright and license terms please see the LICENSE at the root of this
+# distribution (the "License"). All use of this software is governed by the License,
+# or, if provided, by the license below or the license accompanying this file. Do not
+# remove or modify any license notices. This file is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+#
+
+# Extracted from Editor.xml
+set(GEM_DEPENDENCIES
+    Gem::EMotionFX.Editor
+    Gem::GraphModel.Editor
+    Gem::LandscapeCanvas.Editor
+    Gem::GameState
+    Gem::ScriptCanvasPhysics
+    Gem::ScriptEvents.Editor
+    Gem::ScriptCanvasTesting.Editor
+    Gem::Maestro.Editor
+    Gem::ExpressionEvaluation
+    Gem::PhysX.Editor
+    Gem::PhysXDebug.Editor
+    Gem::TextureAtlas
+    Gem::SurfaceData.Editor
+    Gem::DebugDraw.Editor
+    Gem::AudioSystem.Editor
+    Gem::SceneProcessing.Editor
+    Gem::ScriptCanvas.Editor
+    Gem::GraphCanvas.Editor
+    Gem::GradientSignal.Editor
+    Gem::Presence
+    Gem::LocalUser
+    Gem::ImGui.Editor
+    Gem::EditorPythonBindings.Editor
+    Gem::FastNoise.Editor
+    Gem::LyShine.Editor
+    Gem::SaveData
+    Gem::Vegetation.Editor
+    Gem::Camera.Editor
+    Gem::CameraFramework
+    Gem::LmbrCentral.Editor
+    Gem::Atom_RHI.Private
+    Gem::Atom_RPI.Builders
+    Gem::Atom_RPI.Editor
+    Gem::Atom_Feature_Common.Builders
+    Gem::Atom_Feature_Common.Editor
+    Gem::Atom_Bootstrap
+    Gem::Atom_Asset_Shader.Builders
+    Gem::Atom_Component_DebugCamera
+    Gem::AtomImGuiTools
+    Gem::AtomLyIntegration_CommonFeatures.Editor
+    Gem::EMotionFX_Atom.Editor
+    Gem::ImageProcessingAtom.Editor
+    Gem::Atom_AtomBridge.Editor
+    Gem::ImguiAtom
+    Gem::AtomFont
+    Gem::AtomToolsFramework.Editor
+    Gem::GradientSignal.Editor
+    Gem::Multiplayer.Editor
+    Gem::PrefabBuilder
+    Gem::Multiplayer.Tools
+    Gem::MultiplayerSample
+)
