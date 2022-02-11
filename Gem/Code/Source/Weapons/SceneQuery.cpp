b203f26165463eb9ab6efd98106a183f87ad4579
puvvadar <puvvadar@amazon.com>
2022-02-11 09:14:50 -0800

    Switch to higher tolerance physics check during rewind filtering
    Signed-off-by: puvvadar <puvvadar@amazon.com>

--- Gem/Code/Source/Weapons/SceneQuery.cpp
+++ Gem/Code/Source/Weapons/SceneQuery.cpp
@@ -80,9 +80,9 @@ namespace MultiplayerSample
             auto ignoreEntitiesFilterCallback =
                 [&filter, networkEntityManager](const AzPhysics::SimulatedBody* body, [[maybe_unused]] const Physics::Shape* shape)
             {
-                // Exclude the dynamic bodies from another rewind frame
-                if (filter.m_rewindFrameId != Multiplayer::InvalidHostFrameId
-                    && body->GetNativeType() != PhysX::NativeTypeIdentifiers::RigidBodyStatic
+                // Exclude bodies from another rewind frame
+                if (filter.m_rewindFrameId != Multiplayer::InvalidHostFrameId 
+                    && (body->GetFrameId() != AzPhysics::SimulatedBody::UndefinedFrameId)
                      && (body->GetFrameId() != static_cast<uint32_t>(filter.m_rewindFrameId)))
                 {
                     return AzPhysics::SceneQuery::QueryHitType::None;
