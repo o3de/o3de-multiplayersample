5f15a401a836c11fe06ef2d4d1de6dbe7b57dc89
allisaurus <34254888+allisaurus@users.noreply.github.com>
2022-07-18 13:50:36 -0700

    swap tabs for spaces in NetworkTeleportComponent.cpp
    Signed-off-by: allisaurus <34254888+allisaurus@users.noreply.github.com>

--- Gem/Code/Source/Components/NetworkTeleportComponent.cpp
+++ Gem/Code/Source/Components/NetworkTeleportComponent.cpp
@@ -19,123 +19,123 @@
 
 namespace MultiplayerSample
 {
-	void NetworkTeleportComponent::Reflect(AZ::ReflectContext* reflection)
-	{
-		if (auto serializationContext = azrtti_cast<AZ::SerializeContext*>(reflection))
-		{
-			serializationContext->Class<NetworkTeleportComponent, Component>()
-				->Field("Destination", &NetworkTeleportComponent::m_reset)
-				->Version(3);
+    void NetworkTeleportComponent::Reflect(AZ::ReflectContext* reflection)
+    {
+        if (auto serializationContext = azrtti_cast<AZ::SerializeContext*>(reflection))
+        {
+            serializationContext->Class<NetworkTeleportComponent, Component>()
+                ->Field("Destination", &NetworkTeleportComponent::m_reset)
+                ->Version(3);
 
-			if (AZ::EditContext* editContext = serializationContext->GetEditContext())
-			{
-				editContext->Class<NetworkTeleportComponent>("Network Teleport Component",
-					"Teleports colliding player entities to a fixed location")
-					->ClassElement(AZ::Edit::ClassElements::EditorData, "")
-					->Attribute(AZ::Edit::Attributes::Category, 
-						"MultiplayerSample")
-					->Attribute(AZ::Edit::Attributes::AppearsInAddComponentMenu, 
-						AZ_CRC_CE("Game"))
-					->DataElement(0, &NetworkTeleportComponent::m_reset,
-						"Destination", "Entity at which to teleport the colliding player")
-					;
-			}
-		}
-	}
+            if (AZ::EditContext* editContext = serializationContext->GetEditContext())
+            {
+                editContext->Class<NetworkTeleportComponent>("Network Teleport Component",
+                    "Teleports colliding player entities to a fixed location")
+                    ->ClassElement(AZ::Edit::ClassElements::EditorData, "")
+                    ->Attribute(AZ::Edit::Attributes::Category, 
+                        "MultiplayerSample")
+                    ->Attribute(AZ::Edit::Attributes::AppearsInAddComponentMenu, 
+                        AZ_CRC_CE("Game"))
+                    ->DataElement(0, &NetworkTeleportComponent::m_reset,
+                        "Destination", "Entity at which to teleport the colliding player")
+                    ;
+            }
+        }
+    }
 
-	NetworkTeleportComponent::NetworkTeleportComponent()
-		: m_enterTrigger([this](
-			AzPhysics::SimulatedBodyHandle bodyHandle, 
-			const AzPhysics::TriggerEvent& triggerEvent)
-			{
-				this->OnTriggerEnter(bodyHandle, triggerEvent);
-			})
-	{
-	}
+    NetworkTeleportComponent::NetworkTeleportComponent()
+        : m_enterTrigger([this](
+            AzPhysics::SimulatedBodyHandle bodyHandle, 
+            const AzPhysics::TriggerEvent& triggerEvent)
+            {
+                this->OnTriggerEnter(bodyHandle, triggerEvent);
+            })
+    {
+    }
 
-	void NetworkTeleportComponent::Activate()
-	{
-		auto* physicsSystem = AZ::Interface<AzPhysics::SystemInterface>::Get();
-		if (physicsSystem)
-		{
-			auto [sceneHandle, bodyHandle] = physicsSystem->FindAttachedBodyHandleFromEntityId(GetEntityId());
-			AzPhysics::SimulatedBodyEvents::RegisterOnTriggerEnterHandler(
-				sceneHandle, bodyHandle, m_enterTrigger);
-		}
-	}
+    void NetworkTeleportComponent::Activate()
+    {
+        auto* physicsSystem = AZ::Interface<AzPhysics::SystemInterface>::Get();
+         if (physicsSystem)
+        {
+            auto [sceneHandle, bodyHandle] = physicsSystem->FindAttachedBodyHandleFromEntityId(GetEntityId());
+            AzPhysics::SimulatedBodyEvents::RegisterOnTriggerEnterHandler(
+                sceneHandle, bodyHandle, m_enterTrigger);
+        }
+    }
 
-	void NetworkTeleportComponent::OnTriggerEnter(
-		[[maybe_unused]] AzPhysics::SimulatedBodyHandle bodyHandle, const AzPhysics::TriggerEvent& triggerEvent)
-	{
+    void NetworkTeleportComponent::OnTriggerEnter(
+        [[maybe_unused]] AzPhysics::SimulatedBodyHandle bodyHandle, const AzPhysics::TriggerEvent& triggerEvent)
+    {
 
-		if (triggerEvent.m_otherBody)
-		{
-			// TODO: validate it's a player entity
-			AZ::Vector3 teleportLocation = GetDestination();
-			AZ_Printf("Teleporter", "destination point X is: %f\n",
-				teleportLocation.GetX());
+        if (triggerEvent.m_otherBody)
+        {
+            // TODO: validate it's a player entity
+            AZ::Vector3 teleportLocation = GetDestination();
+            AZ_Printf("Teleporter", "destination point X is: %f\n",
+                teleportLocation.GetX());
 
-			AZ::Entity* otherEntity = GetCollidingEntity(triggerEvent.m_otherBody);
-			TeleportPlayer(teleportLocation, otherEntity);
-		}
-	}
+            AZ::Entity* otherEntity = GetCollidingEntity(triggerEvent.m_otherBody);
+            TeleportPlayer(teleportLocation, otherEntity);
+        }
+    }
 
-	AZ::Entity* NetworkTeleportComponent::GetCollidingEntity(AzPhysics::SimulatedBody* collidingBody) const
-	{
-		AZ::Entity* collidingEntity = nullptr;		
-		if (collidingBody)
-		{
-			AZ::EntityId collidingEntityId = collidingBody->GetEntityId();
-			AZ::ComponentApplicationBus::BroadcastResult(
-				collidingEntity, &AZ::ComponentApplicationBus::Events::FindEntity, collidingEntityId);
-		}
-		return collidingEntity;
-	}
+    AZ::Entity* NetworkTeleportComponent::GetCollidingEntity(AzPhysics::SimulatedBody* collidingBody) const
+    {
+        AZ::Entity* collidingEntity = nullptr;		
+        if (collidingBody)
+        {
+            AZ::EntityId collidingEntityId = collidingBody->GetEntityId();
+            AZ::ComponentApplicationBus::BroadcastResult(
+                collidingEntity, &AZ::ComponentApplicationBus::Events::FindEntity, collidingEntityId);
+        }
+        return collidingEntity;
+    }
 
-	AZ::Vector3 NetworkTeleportComponent::GetDestination() const
-	{
-		AZ::Vector3 newLocation = AZ::Vector3::CreateZero();
-		AZ::TransformBus::EventResult(newLocation, m_reset,
-			&AZ::TransformBus::Events::GetWorldTranslation);
+    AZ::Vector3 NetworkTeleportComponent::GetDestination() const
+    {
+        AZ::Vector3 newLocation = AZ::Vector3::CreateZero();
+        AZ::TransformBus::EventResult(newLocation, m_reset,
+            &AZ::TransformBus::Events::GetWorldTranslation);
 
-		return newLocation;
-	}
+        return newLocation;
+    }
 
-	void NetworkTeleportComponent::TeleportPlayer(
-		const AZ::Vector3& v, AZ::Entity* entity)
-	{
-		if (entity)
-		{
-			AZ::EntityId playerToTeleport = entity->GetId();
+    void NetworkTeleportComponent::TeleportPlayer(
+        const AZ::Vector3& v, AZ::Entity* entity)
+    {
+        if (entity)
+        {
+            AZ::EntityId playerToTeleport = entity->GetId();
 
-			AZ_Printf(
-				"Teleporter", "attempting to fling entity: %s\n", "PLAYER");
+            AZ_Printf(
+                "Teleporter", "attempting to fling entity: %s\n", "PLAYER");
 
-			// disable physics so we can move entity
-			Physics::RigidBodyRequestBus::Event(playerToTeleport,
-				&Physics::RigidBodyRequestBus::Events::DisablePhysics);
+            // disable physics so we can move entity
+            Physics::RigidBodyRequestBus::Event(playerToTeleport,
+                &Physics::RigidBodyRequestBus::Events::DisablePhysics);
 
-			// move the entity
-			AZ::TransformBus::Event(playerToTeleport,
-				&AZ::TransformBus::Events::SetWorldTranslation, v);
+            // move the entity
+            AZ::TransformBus::Event(playerToTeleport,
+                &AZ::TransformBus::Events::SetWorldTranslation, v);
 
-			// increment resetCount so it doesn't try to interplate from last location
-			Multiplayer::NetworkTransformComponent* netTransform =
-				entity->FindComponent<Multiplayer::NetworkTransformComponent>();
+            // increment resetCount so it doesn't try to interplate from last location
+            Multiplayer::NetworkTransformComponent* netTransform =
+                entity->FindComponent<Multiplayer::NetworkTransformComponent>();
 
-			if (netTransform) {
-				auto controller = 
-					static_cast<Multiplayer::NetworkTransformComponentController*>(netTransform->GetController());
+            if (netTransform) {
+                auto controller = 
+                    static_cast<Multiplayer::NetworkTransformComponentController*>(netTransform->GetController());
 
-				AZ_Printf(
-					"Teleporter", "updating resetCount to : %u\n", (controller->GetResetCount() + 1));
+                AZ_Printf(
+                    "Teleporter", "updating resetCount to : %u\n", (controller->GetResetCount() + 1));
 
-				controller->SetResetCount(controller->GetResetCount() + 1);
-			}
+                controller->SetResetCount(controller->GetResetCount() + 1);
+            }
 
-			// re-enable physics
-			Physics::RigidBodyRequestBus::Event(playerToTeleport,
-				&Physics::RigidBodyRequestBus::Events::EnablePhysics);
-		}
-	}
+            // re-enable physics
+            Physics::RigidBodyRequestBus::Event(playerToTeleport,
+                &Physics::RigidBodyRequestBus::Events::EnablePhysics);
+        }
+    }
 }
