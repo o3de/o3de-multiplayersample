cfb61e1413394f84e65fd241260e88ddf7f4a5fe
puvvadar <puvvadar@amazon.com>
2023-01-06 15:34:41 -0800

    Add local bot control to NetPlayerMovement
    Signed-off-by: puvvadar <puvvadar@amazon.com>

--- Gem/Code/Source/Components/NetworkPlayerMovementComponent.h
+++ Gem/Code/Source/Components/NetworkPlayerMovementComponent.h
@@ -68,6 +68,19 @@ namespace MultiplayerSample
         NetworkAiComponentController* m_networkAiComponentController = nullptr;
 #endif
 
+#if AZ_TRAIT_CLIENT
+        void UpdateLocalBot();
+        AZ::ScheduledEvent m_updateLocalBot;
+        AZ::SimpleLcgRandom m_botLcg;
+
+        float m_botRemainingTime;
+        float m_botTurnRate;
+        float m_botTargetYawDelta;
+        float m_botTargetPitchDelta;
+        Action m_botAction;
+        bool m_botStrafingRight;
+#endif
+
         // Technically these values should never migrate hosts since they are maintained by the autonomous client
         // But due to how the stress test chaos monkey operates, it puppets these values on the server to mimic a client
         // This means these values can and will migrate between hosts (and lose any stored state)
