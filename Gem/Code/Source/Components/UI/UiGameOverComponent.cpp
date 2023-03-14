37a6e195e44629e73f093c0ed444ad099f058c5c
Gene Walters <genewalt@amazon.com>
2023-03-14 14:47:04 -0700

    Updating Game Over screen to auto-hide itself once the round goes back to 1. Adding textures for the new match countdown from 10 to 0
    Signed-off-by: Gene Walters <genewalt@amazon.com>

--- Gem/Code/Source/Components/UI/UiGameOverComponent.cpp
+++ Gem/Code/Source/Components/UI/UiGameOverComponent.cpp
@@ -10,8 +10,6 @@
 #include <LyShine/Bus/UiElementBus.h>
 #include <LyShine/Bus/UiCursorBus.h>
 #include <LyShine/Bus/UiTextBus.h>
-
-
 #include <Source/Components/UI/UiGameOverComponent.h>
 
 namespace MultiplayerSample
@@ -23,8 +21,9 @@ namespace MultiplayerSample
             serializeContext->Class<UiGameOverComponent, AZ::Component>()
                 ->Version(2)
                 ->Field("Game Over root element", &UiGameOverComponent::m_gameOverRootElement)
-                ->Field("Winner Name Text", &UiGameOverComponent::m_winnerNameElement)
-                ->Field("Match Results element", &UiGameOverComponent::m_matchResultsElement)
+                ->Field("Rank Numbers Elements", &UiGameOverComponent::m_rankNumbersUIContainer)
+                ->Field("Top Player Elements", &UiGameOverComponent::m_topPlayersUIElements)
+                ->Field("Time Remaining Until New Match UI Elements", &UiGameOverComponent::m_timeRemainingUntilNewMatchUIContainer)
                 ;
 
             if (AZ::EditContext* editContext = serializeContext->GetEditContext())
@@ -33,11 +32,13 @@ namespace MultiplayerSample
                     ->ClassElement(AZ::Edit::ClassElements::EditorData, "")
                     ->Attribute(AZ::Edit::Attributes::AppearsInAddComponentMenu, AZ_CRC_CE("CanvasUI"))
                     ->DataElement(AZ::Edit::UIHandlers::Default, &UiGameOverComponent::m_gameOverRootElement,
-                        "Game Over root element", "The root element of all Game Over specific UI")
-                    ->DataElement(AZ::Edit::UIHandlers::Default, &UiGameOverComponent::m_winnerNameElement,
-                        "Winner Name Text", "The text element for the name of the match winner")
-                    ->DataElement(AZ::Edit::UIHandlers::Default, &UiGameOverComponent::m_matchResultsElement,
-                        "Match Results element", "The element for the textual display of the match results")
+                        "Game Over root element", "The root element of all Game Over specific UI.")
+                    ->DataElement(AZ::Edit::UIHandlers::Default, &UiGameOverComponent::m_rankNumbersUIContainer,
+                        "Rank Numbers", "Used to display the rank of this client's autonomous player. The parent container UI element containing ui images for each possible rank. Example 1st-10th place.")
+                    ->DataElement(AZ::Edit::UIHandlers::Default, &UiGameOverComponent::m_topPlayersUIElements,
+                        "Top Players", "A sorted list of rows to display the top players (player rank 1-3). Each row should have 2 children: name and score.")
+                    ->DataElement(AZ::Edit::UIHandlers::Default, &UiGameOverComponent::m_timeRemainingUntilNewMatchUIContainer,
+                        "Time Remaining Until New Match UI Elements", "A container sorted list number images to display the remaining time until the new match starts.")
                     ;
             }
         }
@@ -45,40 +46,49 @@ namespace MultiplayerSample
 
     void UiGameOverComponent::Activate()
     {
+        m_waitForActiveNetworkMatchComponent.Enqueue(AZ::TimeMs{ 1000 }, true);
         UiGameOverBus::Handler::BusConnect(GetEntityId());
     }
 
     void UiGameOverComponent::Deactivate()
     {
         UiGameOverBus::Handler::BusDisconnect();
+        m_waitForActiveNetworkMatchComponent.RemoveFromQueue();
+        m_onTimeRemainingChanged.RemoveFromQueue();
+        m_onRoundNumberChangedHandler.Disconnect();
+    }
 
+    void UiGameOverComponent::DisplayTimeRemainingUI(uint16_t secondsRemaining)
+    {
+        LyShine::EntityArray rankNumberUIElements;
+        UiElementBus::EventResult(rankNumberUIElements, m_timeRemainingUntilNewMatchUIContainer, &UiElementBus::Events::GetChildElements);
+
+        for(uint16_t uiNumbersIdx = 0; uiNumbersIdx < rankNumberUIElements.size(); ++uiNumbersIdx)
+        {
+            UiElementBus::Event(rankNumberUIElements[uiNumbersIdx]->GetId(), &UiElementBus::Events::SetIsEnabled, secondsRemaining == uiNumbersIdx);
+        }
     }
 
     void UiGameOverComponent::SetGameOverScreenEnabled(bool enabled)
     {
-        UiElementBus::Event(m_gameOverRootElement, &UiElementBus::Events::SetIsEnabled, enabled);
-       
-    }
-
-    void UiGameOverComponent::DisplayResults(MatchResultsSummary results)
-    {
-        UiTextBus::Event(m_winnerNameElement, &UiTextBus::Events::SetText, results.m_winningPlayerName.c_str());
-        auto resultsSummary = BuildResultsSummary(results.m_playerStates);
-        UiTextBus::Event(m_matchResultsElement, &UiTextBus::Events::SetText, resultsSummary);
-    }
-
-    AZStd::string UiGameOverComponent::BuildResultsSummary(const AZStd::vector<PlayerState>& playerStates)
-    {
-        // TODO: make this a nice grid in UiCanvas instead of a big string
-        AZStd::string resultTable;
-        for (PlayerState result : playerStates)
+        if (enabled)
         {
-            resultTable.append(
-                AZStd::string::format("%i               %s\n",
-                result.m_score,
-                result.m_playerName.c_str())
-            );
+            m_timeRemainingUntilNewMatch = RestTimeBetweenMatches;
+            DisplayTimeRemainingUI(m_timeRemainingUntilNewMatch);
+            m_onTimeRemainingChanged.Enqueue(AZ::TimeMs{ 1000 }, true);
         }
-        return resultTable;
+        else
+        {
+            m_onTimeRemainingChanged.RemoveFromQueue();
+        }
+
+        UiElementBus::Event(m_gameOverRootElement, &UiElementBus::Events::SetIsEnabled, enabled);
+    }
+
+    void UiGameOverComponent::DisplayResults([[maybe_unused]]MatchResultsSummary results)
+    {
+        //UiTextBus::Event(m_winnerNameElement, &UiTextBus::Events::SetText, results.m_winningPlayerName.c_str());
+        //auto resultsSummary = BuildResultsSummary(results.m_playerStates);
+        //UiTextBus::Event(m_matchResultsElement, &UiTextBus::Events::SetText, resultsSummary);
     }
 }
