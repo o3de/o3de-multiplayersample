e021eea70f5f73a3181b0c2d4694c4d353c81b2d
Mike Balfour <82224783+mbalfour-amzn@users.noreply.github.com>
2023-04-20 13:49:08 -0500

    Add persistent user settings support.
    Signed-off-by: Mike Balfour <82224783+mbalfour-amzn@users.noreply.github.com>

--- Gem/Code/Source/Components/UI/UiSettingsComponent.h
+++ Gem/Code/Source/Components/UI/UiSettingsComponent.h
@@ -12,23 +12,6 @@
 
 namespace MultiplayerSample
 {
-    //! These are all of the user settings that MPS supports.
-    struct MpsSettings
-    {
-        AZ_TYPE_INFO(MpsSettings, "{1E545ABF-6650-41D8-AC69-9C50BB5561F0}");
-        static void Reflect(AZ::ReflectContext* context);
-
-        //! The API type that Atom should use at startup. (This value comes from AZ::RHI::APIIndex)
-        uint32_t m_atomApiType = 0;
-
-        //! The master audio volume (0 - 100). 0 is silent, 100 is max volume.
-        uint8_t m_masterVolume = 100;
-
-        //! The streaming image texture mip bias (0 - N). This affects the max mipmap level that will be loaded for streaming images.
-        //! This doesn't affect other types of images like UI or VFX.
-        uint8_t m_streamingImageMipBias = 1;
-    };
-
     struct UiToggle
     {
         AZ_TYPE_INFO(UiToggle, "{60AD7DDE-1730-41D8-BB82-630FF8008370}");
@@ -61,10 +44,13 @@ namespace MultiplayerSample
         void OnTextureQualityToggle(ToggleDirection toggleDirection);
         void OnMasterVolumeToggle(ToggleDirection toggleDirection);
 
+        template<typename ValueType>
+        static uint32_t GetRotatedIndex(
+            const AZStd::vector<AZStd::pair<ValueType, AZStd::string>>& valuesToLabels,
+            const ValueType& value, ToggleDirection toggleDirection);
+
         UiToggle m_graphicsApiToggle;
         UiToggle m_textureQualityToggle;
         UiToggle m_masterVolumeToggle;
-
-        MpsSettings m_settings;
     };
 }
