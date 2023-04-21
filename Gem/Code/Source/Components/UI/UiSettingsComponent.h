ceb6730de7b12e903ad5d583eb23e4cad5ffb9bf
Mike Balfour <82224783+mbalfour-amzn@users.noreply.github.com>
2023-04-21 17:58:49 -0500

    Add fullscreen/windowed toggle. (#379)
    * Add fullscreen/windowed toggle.
    
    Signed-off-by: Mike Balfour <82224783+mbalfour-amzn@users.noreply.github.com>
    
    * Make the resolution settings active but not exposed.
    
    Signed-off-by: Mike Balfour <82224783+mbalfour-amzn@users.noreply.github.com>
    
    * Added small comment explaining what the class is.
    
    Signed-off-by: Mike Balfour <82224783+mbalfour-amzn@users.noreply.github.com>
    
    ---------
    
    Signed-off-by: Mike Balfour <82224783+mbalfour-amzn@users.noreply.github.com>

--- Gem/Code/Source/Components/UI/UiSettingsComponent.h
+++ Gem/Code/Source/Components/UI/UiSettingsComponent.h
@@ -9,6 +9,7 @@
 
 
 #include <AzCore/Component/Component.h>
+#include <AzFramework/Windowing/WindowBus.h>
 
 namespace MultiplayerSample
 {
@@ -22,8 +23,13 @@ namespace MultiplayerSample
         AZ::EntityId m_rightButtonEntity;
     };
 
+    // This component is attached to the UI Settings screen and handles the logic for changing the user setting values
+    // when the UI toggles are toggled. It activates on level load, not on settings screen navigation as one might think.
+    // On activation, it reapplies all of the MPS user settings while initializing the toggles to help ensure that the
+    // previously-applied settings weren't overridden by the server or by level loads.
     class UiSettingsComponent
         : public AZ::Component
+        , public AzFramework::WindowNotificationBus::Handler
     {
     public:
         AZ_COMPONENT(UiSettingsComponent, "{6F0F5495-E766-444C-808E-4EB91AD891D6}");
@@ -33,6 +39,12 @@ namespace MultiplayerSample
         void Activate() override;
         void Deactivate() override;
     private:
+        // WindowNotificationBus overrides
+        void OnWindowResized(uint32_t width, uint32_t height) override;
+        void OnRefreshRateChanged([[maybe_unused]] uint32_t refreshRate) override;
+
+        static AzFramework::NativeWindowHandle GetWindowHandle();
+
         enum class ToggleDirection
         {
             None,
@@ -40,9 +52,13 @@ namespace MultiplayerSample
             Right
         };
 
-        void OnGraphicsApiToggle(ToggleDirection toggleDirection);
-        void OnTextureQualityToggle(ToggleDirection toggleDirection);
-        void OnMasterVolumeToggle(ToggleDirection toggleDirection);
+        void InitializeToggle(UiToggle& toggle, AZStd::function<void(UiToggle&, ToggleDirection)> toggleUpdateFn);
+
+        static void OnGraphicsApiToggle(UiToggle& toggle, ToggleDirection toggleDirection);
+        static void OnTextureQualityToggle(UiToggle& toggle, ToggleDirection toggleDirection);
+        static void OnMasterVolumeToggle(UiToggle& toggle, ToggleDirection toggleDirection);
+        static void OnFullscreenToggle(UiToggle& toggle, ToggleDirection toggleDirection);
+        static void OnResolutionToggle(UiToggle& toggle, ToggleDirection toggleDirection);
 
         template<typename ValueType>
         static uint32_t GetRotatedIndex(
@@ -52,5 +68,7 @@ namespace MultiplayerSample
         UiToggle m_graphicsApiToggle;
         UiToggle m_textureQualityToggle;
         UiToggle m_masterVolumeToggle;
+        UiToggle m_fullscreenToggle;
+        UiToggle m_resolutionToggle;
     };
 }
