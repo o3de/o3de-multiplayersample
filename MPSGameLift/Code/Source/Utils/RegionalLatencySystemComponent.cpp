136afcf2fa43a56c63285f80b23a788ad8b50d88
AMZN-Gene <genewalt@amazon.com>
2023-07-13 15:27:11 -0700

    Adding copyright headers
    Signed-off-by: AMZN-Gene <genewalt@amazon.com>

--- MPSGameLift/Code/Source/Utils/RegionalLatencySystemComponent.cpp
+++ MPSGameLift/Code/Source/Utils/RegionalLatencySystemComponent.cpp
@@ -1,3 +1,10 @@
+/*
+ * Copyright (c) Contributors to the Open 3D Engine Project.
+ * For complete copyright and license terms please see the LICENSE at the root of this distribution.
+ *
+ * SPDX-License-Identifier: Apache-2.0 OR MIT
+ *
+ */
 
 #include <Utils/RegionalLatencySystemComponent.h>
 
@@ -8,6 +15,7 @@
 #include <AzCore/Serialization/SerializeContext.h>
 #include <AzCore/Serialization/EditContext.h>
 #include <AzCore/RTTI/BehaviorContext.h>
+
 
 namespace MPSGameLift
 {
