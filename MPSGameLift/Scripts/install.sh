690a53eb0eaf09051e12b7a3f6e14e764a98af23
Steve Pham <82231385+spham-amzn@users.noreply.github.com>
2023-10-03 01:02:56 +0000

    Updating export script to support building headless server. Also adding install.sh for gamelift linux servers to install system libraries
    Signed-off-by: Steve Pham <82231385+spham-amzn@users.noreply.github.com>
    Signed-off-by: Gene Walters <genewalt@amazon.com>

---
+++ MPSGameLift/Scripts/install.sh
@@ -0,0 +1,44 @@
+#!/bin/sh
+
+# Place this script in the GameLift server package when uploading to Linux
+# GameLift will run this script before opening the server
+# https://docs.o3de.org/docs/user-guide/gems/reference/aws/aws-gamelift/build-packaging-for-linux/
+
+sudo cp -a ./lib64/libX* /lib64/.
+sudo cp -a ./lib64/libbsd* /lib64/.
+sudo cp -a ./lib64/libstdc++* /lib64/.
+
+if cat /etc/system-release | grep -qFe 'Amazon Linux release 2023'
+then
+    sudo yum update -y
+    sudo yum groupinstall 'Development Tools' -y
+    sudo yum install python3 -y
+    sudo yum -y install libunwind-devel
+
+    echo 'Update outdated make package'
+    mkdir make && cd make
+    wget http://ftp.gnu.org/gnu/make/make-4.2.1.tar.gz
+    tar zxvf make-4.2.1.tar.gz
+    mkdir make-4.2.1-build make-4.2.1-install
+    cd make-4.2.1-build
+    /local/game/make/make-4.2.1/configure --prefix='/local/game/make/gmake-4.2.1-install'
+    make -j 8
+    make -j 8 install
+    export PATH=/local/game/make/gmake-4.2.1-install/bin:$PATH
+    sudo ln -sf /local/game/make/gmake-4.2.1-install/bin/make /local/game/make/gmake-4.2.1-install/bin/gmake
+    cd /local/game/
+
+    echo 'Installing missing libs for AL2023'
+    mkdir glibc && cd glibc
+    wget http://mirror.rit.edu/gnu/libc/glibc-2.29.tar.gz
+    tar zxvf glibc-2.29.tar.gz
+    mkdir glibc-2.29-build glibc-2.29-install
+    cd glibc-2.29-build
+    /local/game/glibc/glibc-2.29/configure --prefix='/local/game/glibc/glibc-2.29-install'
+    make -j 8
+    make -j 8 install
+    sudo ln -sf /local/game/glibc/glibc-2.29-install/lib/libm.so.6 /local/game/lib64/libm.so.6
+    cd /local/game/
+fi
+
+echo 'Install Success'
