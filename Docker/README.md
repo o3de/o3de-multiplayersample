9d50a30dcde32d12691d8c3eb4eee172521d5153
Steve Pham <82231385+spham-amzn@users.noreply.github.com>
2024-05-24 12:30:34 -0700

    Updates
    Signed-off-by: Steve Pham <82231385+spham-amzn@users.noreply.github.com>

--- Docker/README.md
+++ Docker/README.md
@@ -1,26 +1,102 @@
+# Docker support for Multiplayer Sample
+
+## Prerequisites
+
+* [Hardware requirements of o3de](https://www.o3de.org/docs/welcome-guide/requirements/)
+* Any Linux distribution that supports Docker and the NVIDIA container toolkit (see below)
+ * **Note** For the headless server flavor, the NVIDIA container toolkit is not required.
+* At least 60 GB of free disk space
+* Docker installed and configured
+  * **Note** It is recommended to have Docker installed correctly and in a secure manner so that the Docker commands in this guide do not require elevated priviledges (sudo) in order to run them. See [Docker Engine post-installation steps](https://docs.docker.com/engine/install/linux-postinstall/) for more details.
+* [NVidia container toolkit](https://docs.nvidia.com/datacenter/cloud-native/container-toolkit/install-guide.html#docker)
+
+# Building the Docker image
+The Docker scripts accepts arguments to control how to build the Docker image for the Multiplayer Sample. The main argument, `PACKAGE_TYPE` controls the type of launcher that will be built from the Docker build process. There are four different types of launchers supported:
+
+1. **game** : The game client that will connect to a game server.
+2. **server** : The game server that will accept remote game clients and will also provide a display of running game sessions. 
+3. **unified** : The combined game client that also can run as the game server.
+4. **headless** : The game server that will accept remote game clients but will not display and GUI or window. This is launcher is designed to run on headless servers.
 
 
+
+| Argument                | Description                                                                | Default     
+|-------------------------|----------------------------------------------------------------------------|-------------
+| INPUT_IMAGE             | The base ubuntu docker image to base the build on                          | ubuntu:22.04
+| INPUT_ARCHITECTURE      | The CPU architecture (amd64/aarch64). Will require QEMU if cross compiling | amd64
+| O3DE_REPO               | The git repo for O3DE                                                      | https://github.com/o3de/o3de
+| O3DE_BRANCH             | The branch for O3DE                                                        | development
+| O3DE_COMMIT             | The commit on the branch for O3DE (or HEAD)                                | HEAD
+| O3DE_EXTRAS_REPO        | The git repo for O3DE Extras                                               | https://github.com/o3de/o3de-extras
+| O3DE_EXTRAS_BRANCH      | The branch for O3DE Extras                                                 | development
+| O3DE_EXTRAS_COMMIT      | The commit on the branch for O3DE Extras (or HEAD)                         | HEAD
+| O3DE_MPS_REPO           | The git repo for main Multiplayer Sample project                           | https://github.com/o3de/o3de-multiplayersample
+| O3DE_MPS_BRANCH         | The branch for main Multiplayer Sample project                             | development
+| O3DE_MPS_COMMIT         | The commit on the branch for Multiplayer Sample project (or HEAD)          | HEAD
+| O3DE_MPS_ASSETS_REPO    | The git repo for Multiplayer Sample Assets                                 | https://github.com/o3de/o3de-multiplayersample-assets
+| O3DE_MPS_ASSETS_BRANCH  | The branch for Multiplayer Sample Assets                                   | development
+| O3DE_MPS_ASSETS_COMMIT  | The commit on the branch for Multiplayer Sample Assets (or HEAD)           | HEAD
+| RUN_FULLSCREEN          | Option to launch the game, unified, or server in fullscreen mode (0=no, 1=yes) | 0
+
+
+## Examples
+
+### Locally (From the o3de-multiplayersample/Docker folder)
+
+#### Game Launcher
 ```
-xhost +local:docker
+docker build --build-arg PACKAGE_TYPE=game -f Dockerfile -t amd64/o3de-mps-server:latest .
 ```
 
+### Unified Game Launcher
+```
+docker build --build-arg PACKAGE_TYPE=unified -f Dockerfile -t amd64/o3de-mps-server:latest .
+```
 
+### Server Launcher
 ```
 docker build --build-arg PACKAGE_TYPE=server -f Dockerfile -t amd64/o3de-mps-server:latest .
-docker build --build-arg PACKAGE_TYPE=headless-server -f Dockerfile -t amd64/o3de-mps-headless-server:latest .
-docker build --build-arg PACKAGE_TYPE=launcher -f Dockerfile -t amd64/o3de-mps-launcher:latest .
-docker build --build-arg PACKAGE_TYPE=unified-launcher -f Dockerfile -t amd64/o3de-mps-unified-launcher:latest .
 ```
 
+### Headless Server Launcher
+```
+docker build --build-arg PACKAGE_TYPE=headless -f Dockerfile -t amd64/o3de-mps-server:latest .
+```
+
+### From github
+
+#### Game Launcher
+
+```
+docker build --build-arg PACKAGE_TYPE=game -t amd64/o3de-mps-game:latest https://github.com/o3de/o3de-multiplayersample.git#development:Docker
+```
+
+### Unified Game Launcher
+```
+docker build --build-arg PACKAGE_TYPE=unified -t amd64/o3de-mps-unified:latest https://github.com/o3de/o3de-multiplayersample.git#development:Docker
+```
+
+### Server Launcher
+```
+docker build --build-arg PACKAGE_TYPE=server -t amd64/o3de-mps-server:latest https://github.com/o3de/o3de-multiplayersample.git#development:Docker
+```
+
+### Headless Server Launcher
+```
+docker build --build-arg PACKAGE_TYPE=headless -t amd64/o3de-mps-headless:latest https://github.com/o3de/o3de-multiplayersample.git#development:Docker
+```
+
+
+# Running the Docker image
+Running the non-headless Docker images requires Vulkan and GPU acceleration provided by the NVIDIA drivers and container toolkit. The following directions will describe how to launch the Docker containers, utilizing the host Linux machine's X11 display and nvidia drivers, and connecting to the default 'bridge' network. (For advanced network isolation, refer to Docker's command-line reference for [network](https://docs.docker.com/reference/cli/docker/container/run/#network))
+
+```
 xhost +local:root
-
-docker run --rm --network="bridge" --gpus all -e DISPLAY=:1 -v /tmp/.X11-unix:/tmp/.X11-unix -it  o3de-mps-server:latest -c /bin/bash
-docker run --rm --network="bridge" --gpus all -e DISPLAY=:1 -v /tmp/.X11-unix:/tmp/.X11-unix -it  o3de-mps-headless-server:latest -c /bin/bash
-docker run --rm --network="bridge" --gpus all -e DISPLAY=:1 -v /tmp/.X11-unix:/tmp/.X11-unix -it  o3de-mps-launcher:latest -c /bin/bash
-docker run --rm --network="bridge" --gpus all -e DISPLAY=:1 -v /tmp/.X11-unix:/tmp/.X11-unix -it  o3de-mps:o3de-mps-unified-launcher -c /bin/bash
-
-
-
-
-docker build --build-arg PACKAGE_TYPE=launcher -f Dockerfile -t amd64/o3de-mps-launcher:latest .
+docker run --rm --gpus all -e DISPLAY=:1 --network="bridge" -v /tmp/.X11-unix:/tmp/.X11-unix -it $MPS_IMAGE
+```
+`MPS_IMAGE` is variable that can be one of the Docker image name that was used when building the image:
+* **amd64/o3de-mps-game** : The game launcher
+* **amd64/o3de-mps-unified** : The unified game/server launcher
+* **amd64/o3de-mps-server** : The server launcher
+* **amd64/o3de-mps-headless** : The headless server launcher (Does not require GPU acceleration)
 
--- Docker/build.sh
+++ Docker/build.sh
@@ -1,108 +1,127 @@
 #!/bin/bash
+#
+# Copyright (c) Contributors to the Open 3D Engine Project.
+# For complete copyright and license terms please see the LICENSE at the root of this distribution.
+#
+# SPDX-License-Identifier: Apache-2.0 OR MIT
+#
 
-echo WORKSPACE=$WORKSPACE
-echo PACKAGE TYPE=$PACKAGE_TYPE
 
-cd $WORKSPACE
+PROJECT_NAME=MultiplayerSample
 
-################################################
-echo Cloning o3de..
-git clone $O3DE_REPO --single-branch -b $O3DE_BRANCH -o o3de && \
-git -C $WORKSPACE/o3de lfs install && \
-git -C $WORKSPACE/o3de lfs pull && \
-git -C $WORKSPACE/o3de reset --hard $O3DE_COMMIT
+BUILD_ROOT=$WORKSPACE/build
+
+###############################################################################
+# Clone and bootstrap O3DE
+###############################################################################
+git_clone_o3de() {
+    echo "Cloning o3de from $O3DE_REPO/$O3DE_BRANCH into $O3DE_ROOT"
+    git clone --single-branch -b $O3DE_BRANCH $O3DE_REPO $O3DE_ROOT && \
+        git -C $O3DE_ROOT lfs install && \
+        git -C $O3DE_ROOT lfs pull && \
+        git -C $O3DE_ROOT reset --hard $O3DE_COMMIT 
+    if [ $? -ne 0 ]
+    then
+        echo "Error cloning o3de from $O3DE_REPO"
+        exit 1
+    fi
+}
+
+
+
+
+
+
+
+if [ -d $O3DE_ROOT ]
+then
+    # O3DE root exists, keep track of this
+    O3DE_CLONED=0
+else
+    # O3dE root does not exist, clone into the image
+    git_clone_o3de()
+    O3DE_CLONED=1
+fi
+
+$O3DE_ROOT/python/get_python.sh && \
+    $O3DE_ROOT/scripts/o3de.sh register -ep $O3DE_ROOT
 if [ $? -ne 0 ]
 then
-    echo "Unable to clone o3de"
+    echo "Error bootstrapping O3DE from $O3DE_REPO"
     exit 1
 fi
 
 
-################################################
-echo Fetching Python
-$WORKSPACE/o3de/python/get_python.sh 
+###############################################################################
+# Clone and register o3de-extras
+###############################################################################
+echo "Cloning o3de-extras from $O3DE_EXTRAS_REPO/$O3DE_EXTRAS_BRANCH into $O3DE_EXTRAS_ROOT"
+git clone --single-branch -b $O3DE_EXTRAS_BRANCH $O3DE_EXTRAS_REPO $O3DE_EXTRAS_ROOT && \
+    git -C $O3DE_EXTRAS_ROOT lfs install && \
+    git -C $O3DE_EXTRAS_ROOT lfs pull && \
+    git -C $O3DE_EXTRAS_ROOT reset --hard $O3DE_EXTRAS_COMMIT
 if [ $? -ne 0 ]
 then
-    echo "Unable to fetch python"
+    echo "Error cloning o3de-extras from $O3DE_EXTRAS_REPO"
     exit 1
 fi
 
-################################################
-echo Registering o3de
-$WORKSPACE/o3de/scripts/o3de.sh register -ep $WORKSPACE/o3de
-if [ $? -ne 0 ]
-then
-    echo "Unable to register o3de"
-    exit 1
-fi
-
-################################################
-echo Cloning o3de-extras
-git clone $O3DE_EXTRAS_REPO --single-branch -b $O3DE_EXTRAS_BRANCH -o o3de-extras && \
-git -C $WORKSPACE/o3de-extras reset --hard $O3DE_EXTRAS_COMMIT && \
-git -C $WORKSPACE/o3de-extras lfs install && \
-git -C $WORKSPACE/o3de-extras lfs pull 
-if [ $? -ne 0 ]
-then
-    echo "Unable to clone o3de-extras"
-    exit 1
-fi
-
-
-
-################################################
-echo Registering o3de-extras Gems
-$WORKSPACE/o3de/scripts/o3de.sh register --all-gems-path $WORKSPACE/o3de-extras/Gems
+$O3DE_ROOT/scripts/o3de.sh register --all-gems-path $O3DE_EXTRAS_ROOT/Gems
 if [ $? -ne 0 ]
 then
     echo "Unable to register o3de-extras Gems"
     exit 1
 fi
 
-################################################
-echo Cloning o3de-multiplayersample-assets
-git clone $O3DE_MPS_ASSETS_REPO --single-branch -b $O3DE_MPS_ASSETS_BRANCH -o o3de-multiplayersample-assets && \
-git -C $WORKSPACE/o3de-multiplayersample-assets reset --hard $O3DE_MPS_ASSETS_COMMIT && \
-git -C $WORKSPACE/o3de-multiplayersample-assets lfs install && \
-git -C $WORKSPACE/o3de-multiplayersample-assets lfs pull && \
-git -C $WORKSPACE/o3de-multiplayersample-assets submodule update --init --recursive 
+
+###############################################################################
+# Clone and register o3de-multiplayersample-assets
+###############################################################################
+echo "Cloning o3de-multiplayersample-assets from $O3DE_MPS_ASSETS_REPO/$O3DE_MPS_ASSETS_BRANCH into $O3DE_MPS_ASSETS_ROOT"
+git clone --single-branch -b $O3DE_MPS_ASSETS_BRANCH $O3DE_MPS_ASSETS_REPO $O3DE_MPS_ASSETS_ROOT && \
+    git -C $O3DE_MPS_ASSETS_ROOT lfs install && \
+    git -C $O3DE_MPS_ASSETS_ROOT lfs pull && \
+    git -C $O3DE_MPS_ASSETS_ROOT reset --hard $O3DE_MPS_ASSETS_COMMIT && \
+    git -C $O3DE_MPS_ASSETS_ROOT submodule update --init --recursive 
 if [ $? -ne 0 ]
 then
-    echo "Unable to clone o3de-multiplayersample-assets"
+    echo "Unable to clone o3de-multiplayersample-assets from $O3DE_MPS_ASSETS_REPO"
     exit 1
 fi
 
-################################################
-echo Registering o3de-multiplayersample-assets Gems
-$WORKSPACE/o3de/scripts/o3de.sh register --all-gems-path $WORKSPACE/o3de-multiplayersample-assets/Gems
-if [ $? -ne 0 ]
-then
-    echo "Unable to register o3de-multiplayersample-assets Gems"
-    exit 1
-fi
-
-################################################
-echo Cloning o3de-multiplayersample
-git clone $O3DE_MPS_REPO --single-branch -b $O3DE_MPS_BRANCH -o o3de-multiplayersample && \
-git -C $WORKSPACE/o3de-multiplayersample reset --hard $O3DE_EXTRAS_COMMIT && \
-git -C $WORKSPACE/o3de-multiplayersample lfs install && \
-git -C $WORKSPACE/o3de-multiplayersample lfs pull
-if [ $? -ne 0 ]
-then
-    echo "Unable to clone o3de-multiplayersample"
-    exit 1
-fi
-
-################################################
-echo Registering o3de-multiplayersample
-$WORKSPACE/o3de/scripts/o3de.sh register -pp $WORKSPACE/o3de-multiplayersample
+$O3DE_ROOT/scripts/o3de.sh register --all-gems-path $O3DE_MPS_ASSETS_ROOT/Gems
 if [ $? -ne 0 ]
 then
     echo "Unable to register o3de-multiplayersample-assets Gems"
     exit 1
 fi
 
-################################################
+
+###############################################################################
+# Clone and register o3de-multiplayersample
+###############################################################################
+echo "Cloning o3de-multiplayersample from $O3DE_MPS_REPO/$O3DE_MPS_BRANCH into $O3DE_MPS_ROOT"
+git clone --single-branch -b $O3DE_MPS_BRANCH $O3DE_MPS_REPO $O3DE_MPS_ROOT && \
+git -C $O3DE_MPS_ROOT lfs install && \
+git -C $O3DE_MPS_ROOT lfs pull && \
+git -C $O3DE_MPS_ROOT reset --hard $O3DE_MPS_COMMIT
+if [ $? -ne 0 ]
+then
+    echo "Unable to clone o3de-multiplayersample from $O3DE_MPS_REPO"
+    exit 1
+fi
+
+$O3DE_ROOT/scripts/o3de.sh register -pp $O3DE_MPS_ROOT
+if [ $? -ne 0 ]
+then
+    echo "Unable to register the o3de-multiplayersample project"
+    exit 1
+fi
+
+
+###############################################################################
+# Track the git commits from all the repos
+###############################################################################
 echo -e "\n\
 Repository                    | Commit \n\
 ------------------------------+-----------------------------------------\n\
@@ -113,155 +132,162 @@ o3de-multiplayersample        | $O3DE_MPS_REPO/tree/$(git -C $WORKSPACE/o3de-mul
 \n\
 " >> $WORKSPACE/git_commit.txt
 
-################################################
-echo Creating package type $PACKAGE_TYPE
-if [ "$PACKAGE_TYPE" = "server" ]
+###############################################################################
+# Build O3DE tools for asset processing and asset bundling
+###############################################################################
+
+cmake -B $O3DE_MPS_ROOT/build/tools \
+      -S $O3DE_MPS_ROOT \
+      -G "Ninja Multi-Config" \
+      -DLY_DISABLE_TEST_MODULES=ON \
+      -DLY_STRIP_DEBUG_SYMBOLS=ON
+if [ $? -ne 0 ]
 then
-    # Create the 'server' package
-    $WORKSPACE/o3de/scripts/o3de.sh export-project \
-        -es $WORKSPACE/o3de//scripts/o3de/ExportScripts/export_source_built_project.py \
-        -pp $WORKSPACE/o3de-multiplayersample \
-        -ll DEBUG \
-        --config profile \
-        --tool-config profile \
-        --build-tools \
-        --seedlist $WORKSPACE/o3de-multiplayersample/AssetBundling/SeedLists/BasePopcornFxSeedList.seed \
-        --seedlist $WORKSPACE/o3de-multiplayersample/AssetBundling/SeedLists/GameSeedList.seed \
-        --seedlist $WORKSPACE/o3de-multiplayersample/AssetBundling/SeedLists/VFXSeedList.seed \
-        --build-assets \
-        --output-path $WORKSPACE/Export \
-        -a none \
-        --server-project-file-pattern-to-copy $WORKSPACE/o3de-multiplayersample/launch_server.cfg \
-        -nogame \
-        -nounified
-
-    if [ $? -ne 0 ]
-    then
-        echo "Error creating '$PACKAGE_TYPE' package"
-        exit 1
-    fi
-
-    PACKAGE_TARGET=$WORKSPACE/Export/MultiplayerSampleServerPackage#
-    PACKAGE_LAUNCH_TARGET=MultiplayerSample.ServerLauncher
-    ADDITIONAL_LAUNCH_ARG=--console-command-file=launch_server.cfg
-
-
-elif [ "$PACKAGE_TYPE" = "headless-server" ]
-then
-    # Create the 'headless-server' package
-    $WORKSPACE/o3de/scripts/o3de.sh export-project \
-        -es $WORKSPACE/o3de//scripts/o3de/ExportScripts/export_source_built_project.py \
-        -pp $WORKSPACE/o3de-multiplayersample \
-        -ll DEBUG \
-        --config profile \
-        --tool-config profile \
-        --build-tools \
-        --seedlist $WORKSPACE/o3de-multiplayersample/AssetBundling/SeedLists/BasePopcornFxSeedList.seed \
-        --seedlist $WORKSPACE/o3de-multiplayersample/AssetBundling/SeedLists/GameSeedList.seed \
-        --seedlist $WORKSPACE/o3de-multiplayersample/AssetBundling/SeedLists/VFXSeedList.seed \
-        --build-assets \
-        --output-path $WORKSPACE/Export \
-        -a none \
-        --server-project-file-pattern-to-copy $WORKSPACE/o3de-multiplayersample/launch_server.cfg \
-        -nogame \
-        -noserver \
-        --headless-server-launcher \
-        -nounified
-
-    if [ $? -ne 0 ]
-    then
-        echo "Error creating '$PACKAGE_TYPE' package"
-        exit 1
-    fi
-
-    PACKAGE_TARGET=$WORKSPACE/Export/MultiplayerSampleHeadlessServerPackage#
-    PACKAGE_LAUNCH_TARGET=MultiplayerSample.HeadlessServerLauncher
-    ADDITIONAL_LAUNCH_ARG=--console-command-file=launch_server.cfg
-
-elif [ "$PACKAGE_TYPE" = "launcher" ]
-then
-    # Create the 'launcher' package
-    $WORKSPACE/o3de/scripts/o3de.sh export-project \
-        -es $WORKSPACE/o3de//scripts/o3de/ExportScripts/export_source_built_project.py \
-        -pp $WORKSPACE/o3de-multiplayersample \
-        -ll DEBUG \
-        --config profile \
-        --tool-config profile \
-        --build-tools \
-        --seedlist $WORKSPACE/o3de-multiplayersample/AssetBundling/SeedLists/BasePopcornFxSeedList.seed \
-        --seedlist $WORKSPACE/o3de-multiplayersample/AssetBundling/SeedLists/GameSeedList.seed \
-        --seedlist $WORKSPACE/o3de-multiplayersample/AssetBundling/SeedLists/VFXSeedList.seed \
-        --build-assets \
-        --output-path $WORKSPACE/Export \
-        -a none \
-        --game-project-file-pattern-to-copy $WORKSPACE/o3de-multiplayersample/launch_client.cfg \
-        -noserver \
-        -nounified
-
-    if [ $? -ne 0 ]
-    then
-        echo "Error creating '$PACKAGE_TYPE' package"
-        exit 1
-    fi
-
-    PACKAGE_TARGET=$WORKSPACE/Export/MultiplayerSampleGamePackage
-    PACKAGE_LAUNCH_TARGET=MultiplayerSample.GameLauncher
-
-elif [ "$PACKAGE_TYPE" = "unified-launcher" ]
-then
-    # Create the 'unified-launcher' package
-    $WORKSPACE/o3de/scripts/o3de.sh export-project \
-        -es $WORKSPACE/o3de//scripts/o3de/ExportScripts/export_source_built_project.py \
-        -pp $WORKSPACE/o3de-multiplayersample \
-        -ll DEBUG \
-        --config profile \
-        --tool-config profile \
-        --build-tools \
-        --seedlist $WORKSPACE/o3de-multiplayersample/AssetBundling/SeedLists/BasePopcornFxSeedList.seed \
-        --seedlist $WORKSPACE/o3de-multiplayersample/AssetBundling/SeedLists/GameSeedList.seed \
-        --seedlist $WORKSPACE/o3de-multiplayersample/AssetBundling/SeedLists/VFXSeedList.seed \
-        --build-assets \
-        --output-path $WORKSPACE/Export \
-        -a none \
-        -noserver \
-        -nogame
-
-    if [ $? -ne 0 ]
-    then
-        echo "Error creating '$PACKAGE_TYPE' package"
-        exit 1
-    fi
-
-    PACKAGE_TARGET=$WORKSPACE/Export/MultiplayerSampleUnifiedPackage
-    PACKAGE_LAUNCH_TARGET=MultiplayerSample.UnifiedLauncher
-
-else
-    echo "Invalid package type $PACKAGE"
+    echo "Error generating O3DE tools projects"
     exit 1
 fi
 
+cmake --build $O3DE_MPS_ROOT/build/tools \
+      --config profile \
+      --target AssetProcessorBatch AssetBundlerBatch
+if [ $? -ne 0 ]
+then
+    echo "Error building the O3DE tools projects"
+    exit 1
+fi
 
-echo -e "#!/bin/bash\n \
-\n\
-cat $WORKSPACE/git_commit.txt \n\
-if [ "$1" = "console" ]\n\
-then\n\
-    cd $PACKAGE_TARGET\n\
-    ./$PACKAGE_LAUNCH_TARGET --bg_ConnectToAssetProcessor=0 $ADDITIONAL_LAUNCH_ARG\n\
-    exit $?\n\
-else\n\
-    /bin/bash\n\
-    exit $?\n\
-fi\n\
-\n" > $WORKSPACE/launch.sh
+###############################################################################
+# Build the assets for the Multiplayer Sample
+###############################################################################
+pushd $O3DE_MPS_ROOT/build/tools/bin/profile
 
-chmod +x $WORKSPACE/launch.sh
+# Initial run to process the assets
+./AssetProcessorBatch
+
+# Secondary run to re-process ones that missed dependencies
+./AssetProcessorBatch
+
+popd
+
+###############################################################################
+# Bundle the assets for ROSCon2023Demo
+###############################################################################
+
+WORKSPACE_BUNDLE_FOLDER=$WORKSPACE/bundles
+
+if [ "$PACKAGE_TYPE" = "server" ]
+then
+    PACKAGE_FOLDER=$WORKSPACE/MPS_SERVER
+    LAUNCHER_TARGET=ServerLauncher
+elif [ "$PACKAGE_TYPE" = "headless-server" ]
+then
+    PACKAGE_FOLDER=$WORKSPACE/MPS_HEADLESS_SERVER
+    LAUNCHER_TARGET=HeadlessServerLauncher
+elif [ "$PACKAGE_TYPE" = "launcher" ]
+then
+    PACKAGE_FOLDER=$WORKSPACE/MPS_LAUNCHER
+    LAUNCHER_TARGET=GameLauncher
+elif [ "$PACKAGE_TYPE" = "unified-launcher" ]
+then
+    PACKAGE_FOLDER=$WORKSPACE/MPS_UNIFIED_LAUNCHER
+    LAUNCHER_TARGET=UnifiedLauncher
+else
+    echo "Invalid package type: $PACKAGE_TYPE"
+    exit 1
+fi
+
+mkdir -p $WORKSPACE_BUNDLE_FOLDER
+mkdir -p $PACKAGE_FOLDER/Cache/linux
+
+
+###############################################################################
+# Generate the bundles
+###############################################################################
+pushd $O3DE_MPS_ROOT/build/tools/bin/profile
+echo "Creating the game assetList ..."
+./AssetBundlerBatch assetLists \
+        --assetListFile $WORKSPACE_BUNDLE_FOLDER/game_linux.assetList \
+        --platform linux \
+        --project-path $O3DE_MPS_ROOT \
+        --seedlistFile $O3DE_MPS_ROOT/AssetBundling/SeedLists/BasePopcornFxSeedList.seed \
+        --seedlistFile $O3DE_MPS_ROOT/AssetBundling/SeedLists/GameSeedList.seed \
+        --seedlistFile $O3DE_MPS_ROOT/AssetBundling/SeedLists/VFXSeedList.seed \
+        --allowOverwrites
+if [ $? -ne 0 ]
+then
+    echo "Error generating asset list from $WORKSPACE/RosConDemoSeedList.seed"
+    exit 1
+fi
+
+echo "Creating the engine assetList ..."
+./AssetBundlerBatch assetLists \
+        --assetListFile $WORKSPACE_BUNDLE_FOLDER/engine_linux.assetList \
+        --platform linux \
+        --project-path $O3DE_MPS_ROOT \
+        --addDefaultSeedListFiles \
+        --allowOverwrites
+if [ $? -ne 0 ]
+then
+    echo "Error generating default engine asset list"
+    exit 1
+fi
+
+echo "Creating the game asset bundle (pak) ..."
+./AssetBundlerBatch bundles \
+        --platform linux \
+        --project-path $O3DE_MPS_ROOT \
+        --allowOverwrites \
+        --assetListFile $WORKSPACE_BUNDLE_FOLDER/game_linux.assetList \
+        --outputBundlePath $PACKAGE_FOLDER/Cache/linux/game_linux.pak
+if [ $? -ne 0 ]
+then
+    echo "Error bundling generating game pak"
+    exit 1
+fi
+             
+echo "Creating the engine asset bundle (pak) ..."
+./AssetBundlerBatch bundles \
+        --platform linux \
+        --project-path $O3DE_MPS_ROOT \
+        --allowOverwrites \
+        --assetListFile $WORKSPACE_BUNDLE_FOLDER/engine_linux.assetList \
+        --outputBundlePath $PACKAGE_FOLDER/Cache/linux/engine_linux.pak
+if [ $? -ne 0 ]
+then
+    echo "Error bundling generating engine pak"
+    exit 1
+fi
+
+# Build the game launcher monolithically
+echo "Building the ${PROJECT_NAME}.${LAUNCHER_TARGET}."
+cmake -B $O3DE_MPS_ROOT/build/game \
+      -S $O3DE_MPS_ROOT \
+      -G "Ninja Multi-Config" \
+      -DLY_DISABLE_TEST_MODULES=ON \
+      -DLY_STRIP_DEBUG_SYMBOLS=ON \
+      -DLY_MONOLITHIC_GAME=ON \
+      -DALLOW_SETTINGS_REGISTRY_DEVELOPMENT_OVERRIDES=0 \
+&& cmake --build $O3DE_MPS_ROOT/build/game \
+         --config profile \
+         --target ${PROJECT_NAME}.${LAUNCHER_TARGET}
+if [ $? -ne 0 ]
+then
+    echo "Error bundling ${PROJECT_NAME}.${LAUNCHER_TARGET}"
+    exit 1
+fi
+
+cp $O3DE_MPS_ROOT/build/game/bin/profile/${PROJECT_NAME}.${LAUNCHER_TARGET} $PACKAGE_FOLDER/ 
+cp $O3DE_MPS_ROOT/build/game/bin/profile/*.json $PACKAGE_FOLDER/ 
+cp $O3DE_MPS_ROOT/build/game/bin/profile/*.so $PACKAGE_FOLDER
 
 echo Cleaning up data
 rm -rf $WORKSPACE/o3de
 rm -rf $WORKSPACE/o3de-extras
 rm -rf $WORKSPACE/o3de-multiplayersample
 rm -rf $WORKSPACE/o3de-multiplayersample-assets
+rm -rf $HOME/.o3de
+rm -rf $HOME/O3DE
+rm -rf $WORKSPACE_BUNDLE_FOLDER
 
 echo -e "Docker image built from the following repo information\n\n"
 cat $WORKSPACE/git_commit.txt
